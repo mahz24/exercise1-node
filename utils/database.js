44cbe855399375fa399bbb3cdcb0360ee646582a
Marco Zuñiga <marcohurtado358@gmail.com>
2022-05-24 22:06:51 -0500

    version2.0

--- utils/database.js
+++ utils/database.js
@@ -12,7 +12,7 @@ const db = new Sequelize({
    password: process.env.DB_PASSWORD,
    database: process.env.DB,
    logging: false,
-   dialectOptions:
+   dialectOption:
       process.env.NODE_ENV === 'production'
          ? {
               ssl: {
